2912bbeb040ae1e9fd5b55c71d5198b7c721950a
Winter LaMon <winter.lamon@gmail.com>
2017-10-01 19:40:31 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -78,7 +78,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-		 programmer_hash[:yukihiro_matsumoto] = {}
+		 programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP", "C"]}
 		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 		 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 		 programmer_hash[:yukihiro_matsumoto]
