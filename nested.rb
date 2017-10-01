df284ec7822679386345ab8cf7f6d6c34e9f0351
Winter LaMon <winter.lamon@gmail.com>
2017-10-01 19:32:57 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -104,7 +104,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info
 
 end
 
@@ -124,5 +124,9 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+
+		 newly_added_to_hash = "Assembly"
+		 programmer_hash[:dennis_ritchie][:languages] = newly_added_to_hash
+		 return programmer_hash[:dennis_ritchie][:languages]
 
 end
